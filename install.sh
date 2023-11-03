f01b1baa3c22690ac532a96b4818df134b4cfe91
rojberr <rojberr@outlook.com>
2023-11-03 09:18:08 +0100

    build(setup): Use set -ex
    * Print commands and their arguments as they are executed
    * Exit immediately if a command exits with a non-zero status
    
    #NO-TICKET

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -ex
+
 # Install pip for ansible installation
 if command -v python3 >/dev/null 2>&1 && echo "Python 3 is installed"; then
   curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
