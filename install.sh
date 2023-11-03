6cd1ea8136947a1e667e8d0cae5570b3dcf65880
rojberr <rojberr@outlook.com>
2023-11-03 09:37:27 +0100

    build(setup): Add --break-system-packages to python packages install
    * This will install packages in your local user directory, ~/.local/lib/python...
    * I do not use apt provided python packages
    * Therefore I decided to skip this security measure
    
    #NO-TICKET

--- install.sh
+++ install.sh
@@ -5,14 +5,14 @@ set -ex
 # Install pip for ansible installation
 if command -v python3 >/dev/null 2>&1 && echo "Python 3 is installed"; then
   curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-  python3 get-pip.py --user
+  python3 get-pip.py --user --break-system-packages
 else
   echo "ERROR: Install python3"
   exit 1
 fi
 
 # Install ansible, dependencies, roles
-python3 -m pip install --user ansible
+python3 -m pip install --user ansible --break-system-packages
 sudo ansible-galaxy install --force -r roles_to_install.yaml
 
 # Install apps
