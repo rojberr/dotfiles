6ca45abde8e0c8fa15a88c9d30a69f331c7ec4ce
rojberr <rojberr@outlook.com>
2024-03-06 17:03:32 +0100

    install(): Backup scripts before rm + move back conf to scripts/
    #NO-TICKET

--- install.sh
+++ install.sh
@@ -1,35 +1,21 @@
 #!/bin/bash
 set -ex
 
-# Install pip for ansible installation
-# if command -v python3 >/dev/null 2>&1 && echo "Python 3 is installed"; then
-#   curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-#   python3 get-pip.py --user --break-system-packages
-# else
-#   echo "ERROR: Install python3"
-#   exit 1
-# fi
-
-# Install ansible, dependencies, roles
-# python3 -m pip install --user ansible --break-system-packages
-# sudo ansible-galaxy install --force -r roles_to_install.yaml
-
-# Install apps
-# ansible-playbook install-apps.yaml -e ALLOW_ERRORS=no --ask-become-pass
-
 ## Default to BASH
 chsh -s /bin/bash
 
 # Install Brew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-# Load .bash_aliases
+# Backup, create and load .bash_aliases
+mv ~/.bash_aliases ~/.bash_aliases.bak
 rm -f ~/.bash_aliases
-ln -s `pwd`/config/.bash_aliases ~/.bash_aliases
+ln -s `pwd`/shell/.bash_aliases ~/.bash_aliases
 
-# Load bashrc
+# Backup, create and load bashrc
+mv  ~/.bash_rc ~/.bash_rc.bak
 rm -f ~/.bashrc
-ln -s `pwd`/config/.bashrc ~/.bashrc
+ln -s `pwd`/shell/.bashrc ~/.bashrc
 source ~/.bashrc
 
 # # Install Brewfile apps
@@ -50,3 +36,19 @@ helm plugin install https://github.com/jkroepke/helm-secrets
 helm plugin install https://github.com/aslafy-z/helm-git #--version 0.15.1
 
 bash
+
+# Install pip for ansible installation
+# if command -v python3 >/dev/null 2>&1 && echo "Python 3 is installed"; then
+#   curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+#   python3 get-pip.py --user --break-system-packages
+# else
+#   echo "ERROR: Install python3"
+#   exit 1
+# fi
+
+# Install ansible, dependencies, roles
+# python3 -m pip install --user ansible --break-system-packages
+# sudo ansible-galaxy install --force -r roles_to_install.yaml
+
+# Install apps
+# ansible-playbook install-apps.yaml -e ALLOW_ERRORS=no --ask-become-pass
