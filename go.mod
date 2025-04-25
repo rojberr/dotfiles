345b0e7f0edc691b845d9d3c3b3634a854eb04bf
rojberr <rojberr@outlook.com>
2025-04-25 20:24:01 +0200

    feat(go): Add wizard with install selections

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/rojberr/dotfiles
 go 1.24.2
 
 require (
+	github.com/AlecAivazis/survey/v2 v2.3.7
 	github.com/bitfield/script v0.24.1
 	github.com/mgutz/ansi v0.0.0-20200706080929-d51e80ef957d
 )
@@ -10,8 +11,11 @@ require (
 require (
 	github.com/itchyny/gojq v0.12.13 // indirect
 	github.com/itchyny/timefmt-go v0.1.5 // indirect
+	github.com/kballard/go-shellquote v0.0.0-20180428030007-95032a82bc51 // indirect
 	github.com/mattn/go-colorable v0.1.14 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	golang.org/x/sys v0.29.0 // indirect
+	golang.org/x/term v0.8.0 // indirect
+	golang.org/x/text v0.4.0 // indirect
 	mvdan.cc/sh/v3 v3.7.0 // indirect
 )
--- main.go
+++ main.go
@@ -2,11 +2,11 @@ package main
 
 import (
 	"fmt"
-	"strings"
-	"github.com/mgutz/ansi"
+	survey "github.com/AlecAivazis/survey/v2"
 	"github.com/bitfield/script"
+	"github.com/mgutz/ansi"
+	"strings"
 )
-
 
 // Use asciiart logo
 const logo = `
@@ -17,7 +17,19 @@ const logo = `
  \____ |\____/|__|  |__|  |__|____/\___  >____  >
       \/                               \/     \/ `
 
-
+// Wizard with install selections
+var multiQs = []*survey.Question{
+	{
+		Name: "letter",
+		Prompt: &survey.MultiSelect{
+			Message: "What you want to install :",
+			Options: []string{
+				fmt.Sprint("aaa"),
+				fmt.Sprint("bbb"),
+			},
+		},
+	},
+}
 
 func main() {
 
@@ -29,8 +41,18 @@ func main() {
 	// Get the current working directory
 	homedir, _ := script.Exec("bash -c 'echo $HOME'").String()
 	homedir = strings.Replace(homedir, "\n", "", -1)
+	options := []string{}
 
-	fmt.Println(pan + "Welcome to the " + cyan + "Go" + pan + " script" + reset)
-	fmt.Println(homedir)
 	fmt.Println(logo)
+	fmt.Println(pan + "Welcome to the " + cyan + "dotfiles" + pan + " installer script" + reset)
+	fmt.Println("Your homedir: ", homedir)
+
+	// Show wizard with install selections
+	err := survey.Ask(multiQs, &options, survey.WithPageSize(10))
+
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
 }
