a489b1d17c1627f50368c6200da760210ea43b04
rojberr <rojberr@outlook.com>
2025-04-25 20:04:45 +0200

    feat(go): Track go dependencies with go.mod

---
+++ go.mod
@@ -0,0 +1,17 @@
+module github.com/rojberr/dotfiles
+
+go 1.24.2
+
+require (
+	github.com/bitfield/script v0.24.1
+	github.com/mgutz/ansi v0.0.0-20200706080929-d51e80ef957d
+)
+
+require (
+	github.com/itchyny/gojq v0.12.13 // indirect
+	github.com/itchyny/timefmt-go v0.1.5 // indirect
+	github.com/mattn/go-colorable v0.1.14 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	golang.org/x/sys v0.29.0 // indirect
+	mvdan.cc/sh/v3 v3.7.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,7 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+	"github.com/mgutz/ansi"
+	"github.com/bitfield/script"
+)
 
+
+// Use asciiart logo
 const logo = `
       ___      __    _____.__.__                 
    __| _/_____/  |__/ ____\__|  |   ____   ______
@@ -10,6 +17,20 @@ const logo = `
  \____ |\____/|__|  |__|  |__|____/\___  >____  >
       \/                               \/     \/ `
 
+
+
 func main() {
+
+	// Set text colors to use later
+	var pan = ansi.ColorCode("red+bB")
+	var cyan = ansi.ColorCode("cyan+b")
+	var reset = ansi.ColorCode("reset")
+
+	// Get the current working directory
+	homedir, _ := script.Exec("bash -c 'echo $HOME'").String()
+	homedir = strings.Replace(homedir, "\n", "", -1)
+
+	fmt.Println(pan + "Welcome to the " + cyan + "Go" + pan + " script" + reset)
+	fmt.Println(homedir)
 	fmt.Println(logo)
 }
